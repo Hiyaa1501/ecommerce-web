a41e826a172a985f9573dfadb1a785c71261fa7b
hiyaa1501 <hiyabhakta@gmail.com>
2026-04-03 00:25:35 +0530

    added some changes

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO product (name, price, category) VALUES ('Backpack', 5520.00, 'electronics');
-INSERT INTO product (name, price, category) VALUES ('OnePlus Buds', 11999.00, 'electronics');
-INSERT INTO product (name, price, category) VALUES ('Iphone', 90000.00, 'electronics');
-INSERT INTO product (name, price, category) VALUES ('Android Phone', 18000.00, 'electronics');
-INSERT INTO product (name, price, category) VALUES ('Bodycone Dress', 1200.00, 'clothing');
-INSERT INTO product (name, price, category) VALUES ('Top', 600.00, 'clothing');
